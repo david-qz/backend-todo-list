b5e7193ec20122cf50c4d00c53ac5344f4cc63f7
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-08-22 15:56:20 -0700

    Add database set up script

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,17 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+drop table if exists todos;
+drop table if exists users;
+
+create table users (
+    id bigint generated always as identity primary key,
+    email varchar(320) not null unique,
+    password_hash text not null
+);
+
+create table todos (
+    id bigint generated always as identity primary key,
+    user_id bigint references users (id),
+    task text not null,
+    completed boolean not null default false
+);
